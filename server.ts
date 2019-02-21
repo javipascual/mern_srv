adccf9eac2d83045371a45a5a7f6550d385e65f6
Javier Pascual <javipascualg@gmail.com>
2019-02-21 12:12:07 +0100

    Implement event CRUD functions

--- server.ts
+++ server.ts
@@ -2,8 +2,10 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import * as mongoose from "mongoose";
+import { Event } from "./events.model";
 
 const app = express();
+const eventsRoutes = express.Router();
 const PORT = 4000;
 
 app.use(cors());
@@ -15,6 +17,51 @@ connection.once('open', function() {
   console.log("MongoDB database connection established successfully");
 })
 
+eventsRoutes.route('/').get(async function(req, res) {
+  try {
+    const events = await Event.find().exec();
+    res.json(events);
+  }
+  catch(err) {
+    res.status(400).send(err);
+  };
+});
+
+eventsRoutes.route('/add').post(async function(req, res) {
+  try {
+    let event = new Event(req.body);
+    await event.save();
+    res.status(200).send("");
+  }
+  catch(err) {
+    res.status(400).send(err);
+  };
+});
+
+eventsRoutes.route('/edit/:id').put(async function(req, res) {
+  try {
+    const event = await Event.findById(req.params.id).exec();
+    event.date = req.body.date;
+    event.descr = req.body.descr;
+    await event.save();
+    res.json('Event updated!');
+  }
+  catch (err) {
+    res.status(400).send("Update not possible");
+  }
+});
+
+eventsRoutes.route('/delete/:id').delete(async function(req, res) {
+  try {
+    await Event.findByIdAndDelete(req.params.id).exec();
+    res.json('Event deleted!');
+  }
+  catch (err) {
+    res.status(400).send("Deletion not possible");
+  }
+});
+
+app.use('/events', eventsRoutes);
 app.listen(PORT, () =>
   console.log("Server is running on Port: " + PORT)
 );
