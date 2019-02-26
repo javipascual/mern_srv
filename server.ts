53bfc4f92111f50aaeb3c87add6d552b16a0cc5a
Javier Pascual <javipascualg@gmail.com>
2019-02-26 12:16:02 +0100

    Modify mongodb url to connect to mongo docker

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://admin:password@127.0.0.1:27017/events?authSource=admin', { useNewUrlParser: true });
+mongoose.connect('mongodb://admin:password@mongodb:27017/events?authSource=admin', { useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
   console.log("MongoDB database connection established successfully");
